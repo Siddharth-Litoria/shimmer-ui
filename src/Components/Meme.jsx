74736405169116a1ec4d011c1357c541122f3565
Siddharth Litoria <66207577+Siddharth-Litoria@users.noreply.github.com>
2024-08-02 13:22:52 +0530

    Added infinite scroll

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -4,20 +4,38 @@ import Shimmer from './Shimmer';
 
 function Meme() {
     const [meme,setMeme] = useState([]);
+    const [shimmer,setShimmer] = useState(false);
 
     useEffect(()=>{
-        fetchMemes();
+      fetchMemes();
+       window.addEventListener("scroll",handleScroll);
+       return ()=>removeEventListener("scroll",handleScroll);
     },[])
 
+    const handleScroll = ()=>{
+      //scrollY :- How much i have scrolled
+      //window.innerHeight :- Hieght of window visible section
+      //document.body.scrollHeight :- Total height of web page
+      if(window.scrollY + window.innerHeight>=document.body.scrollHeight){
+        fetchMemes();
+        console.log(true);
+      }
+    }
+
     async function fetchMemes(){
-        const data= await fetch("https://meme-api.com/gimme/45");
+      console.log("here in async function")
+      setShimmer(true)
+        const data= await fetch("https://meme-api.com/gimme/20");
         const jsonData= await data.json();
         console.log(jsonData);
-        setMeme(jsonData?.memes);
+        setShimmer(false);
+        setMeme((meme)=>[...meme,...jsonData.memes]);
     }
   return (
-    <div className='flex flex-wrap'>{
-      meme.length>0 ? meme.map((i,k)=><Card key={k} data={i}/>):<Shimmer/>}</div>
+    <div className='flex flex-wrap'> {
+       meme.map((i,k)=><Card key={k} data={i}/>)}
+       {shimmer && <Shimmer/>}
+       </div>
   )
 }
 
